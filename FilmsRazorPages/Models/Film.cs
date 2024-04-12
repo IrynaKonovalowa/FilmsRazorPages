0838b463f349f223de4752fa51d06e888a70c2f6
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-12 08:15:45 +0200

    # Добавлен класс Film

---
+++ FilmsRazorPages/Models/Film.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmsRazorPages.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Le champ doit etre defini")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Le champ doit etre defini")]
+        public string? Genre { get; set; }
+        [Required(ErrorMessage = "Le champ doit etre defini")]
+        public string? Realisateur { get; set; }
+
+        [Required(ErrorMessage = "Le champ doit etre defini")]
+        [Range(1895, 2024, ErrorMessage = "Anne invalide")]
+        public string? Anne { get; set; }
+        [Required(ErrorMessage = "Le champ doit etre defini")]
+        public string? Description { get; set; }
+        public string? Poster { get; set; }
+    }
+}
